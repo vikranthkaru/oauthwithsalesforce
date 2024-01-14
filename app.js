96ff294127b2aaedb1f89fc30484149fed6bc9b4
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-14 14:01:05 +0400

    sf token

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
          loginUrl : process.env['DATABASE_URL']
     });
     var username = process.env['USER_ID'];
-    var password = process.env['USER_PASSWORD'];
+    var password = process.env['USER_PASSWORD']+process.env['SF_TOKEN'];
     conn.login(username, password, function (err, userInfo) {
         if (err) { return console.error(err); }
         // Now you can get the access token and instance URL information.
