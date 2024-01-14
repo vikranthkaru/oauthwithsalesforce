4944aae85fa2dd7997c8a271e05344d90a3d0602
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-14 13:15:51 +0400

    html added

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+
+// Port Number
+const PORT = process.env.PORT ||5000;
+
+fs.readFile('./app.html', function(error,html)
+{
+    if(error) throw error;
+    http.createServer(function(request,response)
+    {
+        response.writeHeader(200,{"Content-Type":"text/html"});
+        response.write(html);
+        response.end();
+    }).listen(PORT)
+});
 const express = require("express");
 const app = express();
 app.get('/', (req, res) => { 
@@ -6,8 +22,6 @@ app.get('/', (req, res) => {
     res.end() 
 }) 
  
-// Port Number
-const PORT = process.env.PORT ||5000;
  
 // Server Setup
 app.listen(PORT,console.log(
