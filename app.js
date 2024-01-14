44f84f22e8714ba9d2cff1cc46b2cab14759d479
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-14 13:43:27 +0400

    4th commit

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-var http = require('http');
-var fs = require('fs');
-
-// Port Number
-const PORT = process.env.PORT ||5000;
-
-fs.readFile('./app.html', function(error,html)
-{
-    if(error) throw error;
-    http.createServer(function(request,response)
-    {
-        response.writeHeader(200,{"Content-Type":"text/html"});
-        response.write(html);
-        response.end();
-    }).listen(PORT)
+var express = require('express'); //Adding Express
+var http = require('http'); //Adding http
+var jsforce = require('jsforce'); //Adding JsForce
+var app = express();
+app.set('port', process.env.PORT || 3001);
+app.get('/', function (req, res) {
+    var conn = new jsforce.Connection({
+         loginUrl : process.env['DATABASE_URL']
+    });
+    var username = process.env['USER_ID'];
+    var password = process.env['USER_PASSWORD'];
+    conn.login(username, password, function (err, userInfo) {
+        if (err) { return console.error(err); }
+        // Now you can get the access token and instance URL information.
+        // Save them to establish a connection next time.
+        console.log(conn.accessToken);
+        console.log(conn.instanceUrl);
+        // logged in user property
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+        res.send('heySalesforce : JSForce Connect Successed!');
+    });
 });
-const express = require("express");
-const app = express();
-app.get('/', (req, res) => { 
-    res.send('A simple Node App is '
-        + 'running on this server') 
-    res.end() 
-}) 
- 
- 
-// Server Setup
-app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
