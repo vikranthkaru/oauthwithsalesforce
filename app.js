249dfd41ee1a9ed2a8d497b608e2fdb80b0a03f2
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-21 14:36:03 +0400

    applied jsforce

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post('/oauthconn',  (req, res) => {
     console.log('Received a form submission');
     console.log(req);
     console.log('url-->' + process.env.DATABASE_URL);
-    const oauth2 = new sf.OAuth2({
+    const oauth2 = new jsforce.OAuth2({
         loginUrl : process.env.DATABASE_URL,
         clientId: process.env.CONSUMER_KEY,
         clientSecret: process.env.CONSUMER_SECRET,
