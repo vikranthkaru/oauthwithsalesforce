e734d45e6e55e75befdfd569c03a3fb46a3e21b4
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-21 19:20:25 +0400

    access token

--- app.js
+++ app.js
@@ -18,25 +18,13 @@ app.get('/', (req, res) => {
 })
 
 app.post('/add',  (req, res) => {
-    console.log('Received a form submission');
-    console.log(req);
-    console.log('url-->' + process.env.DATABASE_URL);
     var conn = new jsforce.Connection({
         loginUrl: process.env.DATABASE_URL
     });
     var username = process.env.USER_NAME;
-    console.log('username-->' + username);
     var password = process.env.USER_PASSWORD + process.env.SF_TOKEN;
-    console.log('password-->' + password);
     conn.login(username, password, function (err, userInfo) {
         if (err) { return console.error(err); }
-        // Now you can get the access token and instance URL information.
-        // Save them to establish a connection next time.
-        console.log(conn.accessToken);
-        console.log(conn.instanceUrl);
-        // logged in user property
-        console.log("User ID: " + userInfo.id);
-        console.log("Org ID: " + userInfo.organizationId);
         fs.readFile('./webflow.html', (error, html) => {
             if (error) {
                 res.status(500).send('Internal Server Error');
@@ -57,8 +45,6 @@ function base64UrlEscape(str) {
 app.post('/oauthconn',  (req, res) => {
     const codeVerifier = base64UrlEscape(crypto.randomBytes(32).toString('base64'));
     const codeChallenge = base64UrlEscape(crypto.createHash('sha256').update(codeVerifier).digest('base64'));
-    console.log('codeVerifier--->'+codeVerifier);
-    console.log('codeChallenge--->'+codeChallenge);
     const oauth2 = new jsforce.OAuth2({
         clientId: process.env.CONSUMER_KEY,
         clientSecret: process.env.CONSUMER_SECRET,
@@ -67,8 +53,14 @@ app.post('/oauthconn',  (req, res) => {
         redirectUri: process.env.REDIRECT_URI,
     });
     const authorizationUrl = oauth2.getAuthorizationUrl({}) + `&code_challenge=${encodeURIComponent(codeChallenge)}`;
-    console.log('Authorization URL: ' + authorizationUrl);
-    res.redirect(authorizationUrl);
+    const fetchAccessToken = new jsforce.Connection({ oauth2: authorizationUrl });
+    fetchAccessToken.authorize(req.query.code, function(err, userInfo){
+        if (err) {
+            return console.error(err);
+          }
+          console.log(conn.accessToken, conn.instanceUrl); 
+    });
+    // res.redirect(authorizationUrl);
     //res.send('heySalesforce : JSForce Connect Successed!');
 });
 
