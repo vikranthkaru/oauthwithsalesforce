a532f0a216e3068bdb9b5cf6ebf5bc97fb3e39bd
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-21 22:12:53 +0400

    add app js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var jsforce = require('jsforce');
 var fs = require('fs');
-var ejs = require('ejs'); 
+var ejs = require('ejs');
 var app = express();
 app.set('port', process.env.PORT || 3001);
 app.use(express.urlencoded({ extended: true }));
@@ -34,7 +34,6 @@ app.get('/oauthcallback', function (req, res) {
         clientSecret: process.env.CONSUMER_SECRET,
         redirectUri: process.env.REDIRECT_URI
     });
-    console.log('codeVerifier-->'+codeVerifier);
     const conn = new jsforce.Connection({ oauth2: oauth2 });
     conn.authorize(req.query.code, { code_verifier: codeVerifier }, function (err, userInfo) {
         if (err) {
@@ -43,27 +42,23 @@ app.get('/oauthcallback', function (req, res) {
         instantceURL = conn.instanceUrl;
         accessToken = conn.accessToken;
         const conn2 = new jsforce.Connection({
-            instanceUrl : conn.instanceUrl,
-            accessToken : conn.accessToken
+            instanceUrl: conn.instanceUrl,
+            accessToken: conn.accessToken
         });
-        conn2.identity(function(err, sfUserInfo) {
+        conn2.identity(function (err, sfUserInfo) {
             if (err) { return console.error(err); }
             const data = {
                 userName: sfUserInfo.username,
                 displayname: sfUserInfo.display_name
             };
             const response = res;
-            console.log("user ID: " + res.user_id);
-            console.log("organization ID: " + res.organization_id);
-            console.log("username: " + res.username);
-            console.log("display name: " + res.display_name);
             fs.readFile('./webflow.html', (error, html) => {
                 if (error) {
                     res.status(500).send('Internal Server Error');
                 } else {
-                   // res.setHeader('Content-Type', 'text/html');
+                    // res.setHeader('Content-Type', 'text/html');
                     response.render('pages/index', { data });
-                   // res.status(200).send(html);
+                    // res.status(200).send(html);
                 }
             });
         });
