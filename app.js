1c182bcbd6e55865879c8fc1f38d8e404ba7fc7d
vikranthkaru <vickyvikranth444@gmail.com>
2024-01-21 21:17:06 +0400

    added html value

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var jsforce = require('jsforce');
 var fs = require('fs');
-//const PORT = process.env.PORT ||5000; s
+var ejs = require('ejs'); 
 var app = express();
 app.set('port', process.env.PORT || 3001);
 app.use(express.urlencoded({ extended: true }));
@@ -25,8 +25,9 @@ function base64UrlEscape(str) {
 
 const codeVerifier = base64UrlEscape(crypto.randomBytes(32).toString('base64'));
 const codeChallenge = base64UrlEscape(crypto.createHash('sha256').update(codeVerifier).digest('base64'));
-
-app.get('/oauthcallback', function(req,res) {
+var instantceURL;
+var accessToken;
+app.get('/oauthcallback', function (req, res) {
     fs.readFile('./webflow.html', (error, html) => {
         if (error) {
             res.status(500).send('Internal Server Error');
@@ -35,30 +36,35 @@ app.get('/oauthcallback', function(req,res) {
             res.status(200).send(html);
         }
     });
-    // const oauth2 = new jsforce.OAuth2({
-    //   clientId: process.env.CONSUMER_KEY,
-    //   clientSecret: process.env.CONSUMER_SECRET,
-    //   redirectUri: process.env.REDIRECT_URI
-    // });
-    // const conn = new jsforce.Connection({ oauth2 : oauth2 });
-    // conn.authorize(req.query.code,  { code_verifier: codeVerifier }, function(err, userInfo) {
-    //   if (err) {
-    //     return console.error(err);
-    //   }
-    //   const conn2 = new jsforce.Connection({
-    //     instanceUrl : conn.instanceUrl,
-    //     accessToken : conn.accessToken
-    //   });
-    //   conn2.identity(function(err, res) {
-    //     if (err) { return console.error(err); }
-    //     console.log("user ID: " + res.user_id);
-    //     console.log("organization ID: " + res.organization_id);
-    //     console.log("username: " + res.username);
-    //     console.log("display name: " + res.display_name);
-    //     res.send('heySalesforce : JSForce Connect Successed!');
-    //   });
-    // });
-  });
+    const oauth2 = new jsforce.OAuth2({
+        clientId: process.env.CONSUMER_KEY,
+        clientSecret: process.env.CONSUMER_SECRET,
+        redirectUri: process.env.REDIRECT_URI
+    });
+    const conn = new jsforce.Connection({ oauth2: oauth2 });
+    conn.authorize(req.query.code, { code_verifier: codeVerifier }, function (err, userInfo) {
+        if (err) {
+            return console.error(err);
+        }
+        instantceURL = conn.instanceUrl;
+        accessToken = conn.accessToken;
+        const conn2 = new jsforce.Connection({
+            instanceUrl : conn.instanceUrl,
+            accessToken : conn.accessToken
+        });
+        conn2.identity(function(err, res) {
+            if (err) { return console.error(err); }
+            const data = { userName: res.username,
+            displayname: res.display_name};
+            res.render('app.ejs', { data }); 
+            console.log("user ID: " + res.user_id);
+            console.log("organization ID: " + res.organization_id);
+            console.log("username: " + res.username);
+            console.log("display name: " + res.display_name);
+            //res.send('heySalesforce : JSForce Connect Successed!');
+        });
+    });
+});
 
 function makeCallout(accessToken) {
     // Example: Making a callout to the Salesforce REST API
@@ -68,18 +74,18 @@ function makeCallout(accessToken) {
             'Authorization': `Bearer ${accessToken}`
         }
     })
-    .then(response => {
-        console.log('API Response:', response.data);
-        // Handle the response from the Salesforce API
-    })
-    .catch(error => {
-        console.error('API Error:', error);
-        // Handle errors
-    });
+        .then(response => {
+            console.log('API Response:', response.data);
+            // Handle the response from the Salesforce API
+        })
+        .catch(error => {
+            console.error('API Error:', error);
+            // Handle errors
+        });
 }
 
 
-app.post('/oauthconn',  (req, res) => {
+app.post('/oauthconn', (req, res) => {
     const oauth2 = new jsforce.OAuth2({
         clientId: process.env.CONSUMER_KEY,
         clientSecret: process.env.CONSUMER_SECRET,
@@ -88,7 +94,7 @@ app.post('/oauthconn',  (req, res) => {
         redirectUri: process.env.REDIRECT_URI,
     });
     const authorizationUrl = oauth2.getAuthorizationUrl({}) + `&code_challenge=${encodeURIComponent(codeChallenge)}`;
-      res.redirect(authorizationUrl);
+    res.redirect(authorizationUrl);
 });
 
 http.createServer(app).listen(app.get('port'), () => {
